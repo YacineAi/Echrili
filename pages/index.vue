ce6047d319d932db4c0723f0c2986929197e12c4
Yacine Djenidi <djenidi.dev@gmail.com>
2024-03-13 03:17:46 +0100

    add

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <template>
   <!-- Start Header  -->
+   <!--
   <div class="header flex justify-center items-center m-10">
     <img class="w-20" src="~/assets/images/logo.png" alt="Echrili Store">
     <h1 class="text-2xl font-bold ml-5">Echrili Store</h1>
   </div>
+  -->
+  <div class="header flex flex-col justify-center items-center m-5">
+    <img class="w-90 lg:w-11/12" src="~/assets/images/logoramadan.png" alt="Echrili Store">
+    <img class="w-80 mt-3 lg:w-6/12" src="~/assets/images/ramdankarim.png">
+</div>
   <!-- End Header  -->
   <div class="flex flex-col justify-center">
     <div class="text-center mb-20">
@@ -90,6 +96,7 @@
             <h2 class="text-xl font-bold text-gray-800">Total : {{ calcTotal() }}</h2>
           </div>
           <p class="text-center text-xs">سعر المنتج + الشحن * سعر الدولار + العمولة</p>
+          <p class="text-center text-white bg-[#424242] rounded-md mt-4" dir="rtl">لتاكيد عملية الشراء، التقط صورة Capture وارسلها لحسابنا في ميسانجر او واتساب</p>
           <div class="flex justify-center space-x-7 mt-4">
                 <a href="https://m.me/CHRiLiStore" class="px-4 py-2 rounded-lg bg-blue-500 text-white font-bold hover:bg-blue-600 focus:ring-2 focus:ring-blue-400"> Messenger</a>
                 <a href="https://wa.me/+213656323285" class="px-4 py-2 rounded-lg bg-green-400 text-white font-bold hover:bg-green-500 focus:ring-2 focus:ring-blue-400"> WhatsApp</a>
@@ -110,6 +117,7 @@
             <div class="text-center absolute bottom-0 right-0">
               <h2 class="text-xl font-bold text-gray-800">Total : {{ calcTotal() }}</h2>
               <p class="text-xs">سعر المنتج + الشحن * سعر الدولار + العمولة</p>
+              <p class="text-center text-white bg-[#424242] rounded-md mt-4" dir="rtl">لتاكيد عملية الشراء، التقط صورة Capture وارسلها لحسابنا في ميسانجر او واتساب</p>
               <div class="flex justify-center space-x-7 mt-4">
                 <a href="https://m.me/CHRiLiStore" class="px-4 py-2 rounded-lg bg-blue-500 text-white font-bold hover:bg-blue-600 focus:ring-2 focus:ring-blue-400"> Messenger</a>
                 <a href="https://wa.me/+213656323285" class="px-4 py-2 rounded-lg bg-green-400 text-white font-bold hover:bg-green-500 focus:ring-2 focus:ring-blue-400"> WhatsApp</a>
@@ -178,9 +186,12 @@
         <!-- -->
         </div>
       </div>
+      <p class="text-center font-bold text-red-600" dir="rtl">رابط السوبر & رابط النقاط & رابط الكوبونات
+يتم حسابها كراوبط عادية في الموقع</p>
       <div v-if="cardData && !isLoading" class="flex flex-col items-center md:gap-y-5 px-7 p-5 text-center" dir="rtl">
       <span class="bg-red-500 font-bold text-white px-2 py-2 rounded">ملاحظة</span>
-      <p class="text-xl">هذا السعر النهائي خاضع لعمولة الخدمة في <span class="font-bold">Echrili Store</span> + تكاليف الشحن و التوصيل من الصين لمحل سكنك في الجزائر. يمكنك معرفة طريقة حساب السعر عبر جدول العمولات و هذا لتفادي الوقوع في شبهة <span class="font-bold">بيع ما لا نملك</span></p>
+      <p class="text-xl">هذا السعر نهائي خاضع لعمولة الخدمة في <span class="font-bold">Echrili Store</span> + تكاليف الشحن و التوصيل من الصين لمحل سكنك في الجزائر.</p>
+      <p class="text-xl">يمكنك معرفة طريقة حساب السعر عبر جدول العمولات و هذا لتفادي الوقوع في شبهة <span class="font-bold">بيع ما لا نملك</span></p>
     </div>
     </div>
 
@@ -189,6 +200,7 @@
   <div class="bot-t text-center mt-4 flex flex-col items-center">
     <span class="bot-text mb-5 font-bold text-1xl bg-[#22CF1B]	w-fit	 rounded-md ">إستعمل هذا البوت للحصول على أسعار أقل</span>
     <a href="https://t.me/Echrili_Bot" ><svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 256 256"><defs><linearGradient id="logosTelegram0" x1="50%" x2="50%" y1="0%" y2="100%"><stop offset="0%" stop-color="#2AABEE"/><stop offset="100%" stop-color="#229ED9"/></linearGradient></defs><path fill="url(#logosTelegram0)" d="M128 0C94.06 0 61.48 13.494 37.5 37.49A128.038 128.038 0 0 0 0 128c0 33.934 13.5 66.514 37.5 90.51C61.48 242.506 94.06 256 128 256s66.52-13.494 90.5-37.49c24-23.996 37.5-56.576 37.5-90.51c0-33.934-13.5-66.514-37.5-90.51C194.52 13.494 161.94 0 128 0"/><path fill="#FFF" d="M57.94 126.648c37.32-16.256 62.2-26.974 74.64-32.152c35.56-14.786 42.94-17.354 47.76-17.441c1.06-.017 3.42.245 4.96 1.49c1.28 1.05 1.64 2.47 1.82 3.467c.16.996.38 3.266.2 5.038c-1.92 20.24-10.26 69.356-14.5 92.026c-1.78 9.592-5.32 12.808-8.74 13.122c-7.44.684-13.08-4.912-20.28-9.63c-11.26-7.386-17.62-11.982-28.56-19.188c-12.64-8.328-4.44-12.906 2.76-20.386c1.88-1.958 34.64-31.748 35.26-34.45c.08-.338.16-1.598-.6-2.262c-.74-.666-1.84-.438-2.64-.258c-1.14.256-19.12 12.152-54 35.686c-5.1 3.508-9.72 5.218-13.88 5.128c-4.56-.098-13.36-2.584-19.9-4.708c-8-2.606-14.38-3.984-13.82-8.41c.28-2.304 3.46-4.662 9.52-7.072"/></svg></a>
+    <p class="text-xl" dir="rtl">للحصول على تجربة تسوق مثالية ننصحكم بتثبيت تطبيق <span class="font-bold">AliExpress</span> وتحويل عملة الموقع الى الاورو او الدولار</p>
   </div>
   <!-- End Bot Telegram  -->
   <!-- More Services  -->
