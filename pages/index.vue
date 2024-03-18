f00d57cc4b81005aba2e91d08064117b36b38bdf
Yacine Djenidi <djenidi.dev@gmail.com>
2024-03-18 19:47:08 +0100

    alert

--- pages/index.vue
+++ pages/index.vue
@@ -186,6 +186,17 @@
         <!-- -->
         </div>
       </div>
+      <a href="https://web.facebook.com/profile.php?id=61557389585178">
+  <div role="alert" class="max-w-[300px] lg:max-w-[760px] p-2 bg-red-500 rounded-xl text-white leading-none lg:rounded-xl">
+    <div class="flex flex-col items-start items-center">
+      <span dir="ltr" class="flex rounded-full bg-white text-red-500 px-2 p-1 text-xs mb-2">هام</span>
+      <p class="text-center text-white leading-relaxed p-2" dir="rtl">نعتذر لكم متابعينا الأعزاء عن إختفاء صفحتنا الأولى التي تضم أكثر من 27الف متابع بسبب الأبلاغات تم حذفها من طرف الفيسبوك ! نحن نعمل على إستعادتها الان. نرجو منكم الضغط على النافذة التالية للوصول الى صفحتنا الجديدة.</p>
+    
+      <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="fill-current opacity-75 h-4 w-4"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"></path></svg>
+    </div>
+  </div>
+</a>
+
       <p class="text-center font-bold text-red-600" dir="rtl">رابط السوبر & رابط النقاط & رابط الكوبونات
 يتم حسابها كراوبط عادية في الموقع</p>
       <div v-if="cardData && !isLoading" class="flex flex-col items-center md:gap-y-5 px-7 p-5 text-center" dir="rtl">
