4499ecf288398a38fc32bffbcd1360843cf7fceb
Yacine Djenidi <djenidi.dev@gmail.com>
2024-07-25 05:20:18 +0100

    add

--- server/api/get.js
+++ server/api/get.js
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => {
     const { url } = getQuery(event)
     
-    const data = await $fetch(`https://alifetcher.onrender.com/detail?id=${url}`);
+    const data = await $fetch(`https://asiafetcher.onrender.com/detail?id=${url}`);
 
     const details = { ...data };
     
